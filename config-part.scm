f0b6e67bf1335b8d14b786481188602f7df68282
Robert McAtee <rtmcatee@gmail.com>
2022-01-13 13:31:10 -0600

    Delete config-part.scm

--- config-part.scm
+++
@@ -1,9 +0,0 @@
-  (services (modify-services %desktop-services
-             (guix-service-type config => (guix-configuration
-               (inherit config)
-               (substitute-urls
-                (append (list "https://substitutes.nonguix.org")
-                  %default-substitute-urls))
-               (authorized-keys
-                (append (list (local-file "./signing-key.pub"))
-                  %default-authorized-guix-keys))))))
